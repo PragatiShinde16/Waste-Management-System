888ff034ec1de79458c4495f0b7db5c57447aa2d
Pragati Shinde <pragatishinde491@gmail.com>
2024-07-02 12:45:48 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,57 @@
+ <?php
+   include_once('./includes/headerNav.php')
+?>
+<!-- Header End====================================================================== -->
+
+<!-- MainBody End ============================= -->
+
+<html>
+<head>
+   <style>
+
+   </style>
+</head>
+<body>
+<div class="card">
+  <div class="card-body">
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title">
+          </div>
+  
+          <div class="row content">
+            <div class="col-lg-6">
+            <p>The ‘WASTE MANAGEMENT SYSTEM is a web application aimed to provide a digital way of complaining the concerns of general citizens to their relative municipalities. </p>
+              <ul>
+                <li><i class="ri-check-double-line"></i> Complaining about waste or garbage problems near their locality. </li>
+                <li><i class="ri-check-double-line"></i> See their complain Report and check if the work is done! or not. </li>
+                <li><i class="ri-check-double-line"></i> people can take different ideas regarding recycling of waste through this website. </li>
+              </ul>
+            </div>
+            <div class="col-lg-6 pt-4 pt-lg-0">
+              <p>Complaining about the waste problem encountered everyday to municipality is hefty process and waste management
+                 aims to make this process easier. With a simple handheld device with access to internet, user can use this platform complain 
+                 their concerns to municipality . The automated system will redirect the complains .The municipality admins at the receiving side can acknowledge
+                  the reports which lets the users whether their complain is adddressed or not. </p>
+              <a href="#" class="btn-learn-more">Learn More</a>
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- End About Us Section -->
+  </div>
+</div>
+
+<!-- Footer ================================================================== -->
+<?php
+   include_once('./includes/footer.php')
+?>
+
+<!-- Placed at the end of the document so the pages load faster ============================================= -->
+<script src="./js/jquery.js" type="text/javascript"></script>
+<script src="./js/bootstrap.min.js" type="text/javascript"></script>
+
+</body>
+</html>
